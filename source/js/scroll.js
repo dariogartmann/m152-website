eb579341701f31aae5aa0e1e0d1e50b9077a5db4
dariogartmann <mail@dariogartmann.ch>
2017-10-10 21:53:54 +0200

    added lumos and images

--- source/js/scroll.js
+++ source/js/scroll.js
@@ -1,26 +1,28 @@
 $('a[href*="#"].scroll').not('[href="#"]').not('[href="#0"]').click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 300, function() {
-          // bla;
-        });
-      }
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 300, function() {
+        // bla;
+      });
     }
-  });
+  }
+});
+
 
 $(document).ready(function() {
+
     $(window).scroll( function() {
         var scroll = $(window).scrollTop();
         var speed = 0.5;
